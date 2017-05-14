78968c380a0231a50b976bc8be071fe0fad5d494
leon20121005 <leon20121005@gmail.com>
2017-05-14 19:55:53 +0800

    improve layout and start parsing another website

--- app/src/main/java/com/example/leon/jsoup/MainActivity.java
+++ app/src/main/java/com/example/leon/jsoup/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.text.method.ScrollingMovementMethod;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -15,7 +16,8 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity
 {
-    private Button _button;
+    private Button _parseButton;
+    private Button _clearButton;
     private TextView _textView;
 
     @Override
@@ -24,13 +26,23 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         _textView = (TextView) findViewById(R.id.textView);
-        _button = (Button) findViewById(R.id.button);
-
-        _button.setOnClickListener(new View.OnClickListener() {
+        _textView.setMovementMethod(new ScrollingMovementMethod());
+        _parseButton = (Button) findViewById(R.id.button2);
+        _parseButton.setOnClickListener(new View.OnClickListener()
+        {
             @Override
             public void onClick(View view)
             {
                 GetWebSite();
+            }
+        });
+        _clearButton = (Button) findViewById(R.id.button);
+        _clearButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                _textView.setText("Cleared");
             }
         });
     }
@@ -46,15 +58,20 @@ public class MainActivity extends AppCompatActivity
 
                 try
                 {
-                    Document doc = Jsoup.connect("http://www.ssaurel.com/blog").get();
+                    Document doc = Jsoup.connect("http://www.ipeen.com.tw/search/taiwan/000/1-0-0-0/").get();
                     String title = doc.title();
-                    Elements links = doc.select("a[href]");
+//                    Elements links = doc.select("a[href]");
+                    Elements elements = doc.select("span");
 
                     builder.append(title).append("\n");
 
-                    for (Element link : links)
+//                    for (Element link : links)
+//                    {
+//                        builder.append("\n").append("Link: ").append(link.attr("href")).append("\n").append("Text: ").append(link.text());
+//                    }
+                    for (Element element : elements)
                     {
-                        builder.append("\n").append("Link: ").append(link.attr("href")).append("\n").append("Text: ").append(link.text());
+                        builder.append("\n").append("Text: ").append(element.text());
                     }
                 }
                 catch (IOException e)
